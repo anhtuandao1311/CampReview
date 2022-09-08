466bce7797f2c3fe24d707c561bf02528746bb65
anhtuandao1311 <106532352+anhtuandao1311@users.noreply.github.com>
2022-09-08 18:31:55 +0700

    Add error class and basic layouts

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0;i<50;i++){
         const ran = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
             location:`${cities[ran].city}, ${cities[ran].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image:'https://source.unsplash.com/collection/483251',
+            description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci iure modi enim. Velit doloribus laborum totam fugit dicta laudantium fugiat. In animi quis culpa repellat quam ab voluptates obcaecati itaque!',
+            price:price
         })
         await camp.save();
 
